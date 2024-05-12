650e630e96660d9471003a8a0f2faedee999421c
alex-kukhtin <alex.kukhtin@outlook.com>
2024-05-12 08:26:18 +0300

    Add project files.

---
+++ WebApp/Startup.cs
@@ -0,0 +1,25 @@
+﻿// Copyright © 2024 Oleksandr Kukhtin. All rights reserved.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp;
+
+public class Startup(IConfiguration configuration)
+{
+	public IConfiguration Configuration { get; } = configuration;
+
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.UseAppRuntimeBuilder(); // Before platform!
+
+		services.UsePlatform(Configuration);
+	}
+
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+	{
+		app.ConfigurePlatform(env, Configuration);
+	}
+}
